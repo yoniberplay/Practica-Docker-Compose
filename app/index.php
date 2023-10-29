be39eb260ed0bf852abcb91cfb28f70269a363fc
yencarnacion07 <yoniber.encarnacion@gmail.com>
2023-10-28 22:08:18 -0400

    init

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Practica Docker Compose</title>
+</head>
+<body>
+    <h1 >Practica Docker Compose</h1>
+    <p>S8: Contenedores y Orquestacion de Contenedores.</p>
+
+
+
+<?php
+        $student = getenv("ESTUDIANTE");
+        $mat = getenv("MAT");
+        echo "<p> <b>Estudiante: </b> $student </p>";
+        echo "<p> <b>Matricula: </b> $mat </p>";
+    ?>
+   
+
+</body>
+</html>
